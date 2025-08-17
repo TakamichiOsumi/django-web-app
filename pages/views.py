245116f782faf1a2a96cf504012ac7b909aedb2b
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-08-17 22:24:36 +0900

    Show the team members at the 'home' page.

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Team
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    team = Team.objects.all()
+    data = { 'team' : team }
+    return render(request, 'pages/home.html', data)
 
 def about(request):
     return render(request, 'pages/about.html')
